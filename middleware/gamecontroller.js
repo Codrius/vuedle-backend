1099a2ab8412573b4464f34d85d69ea7fd06efb9
Codrius <magefire342@gmail.com>
2023-08-18 21:17:49 -0400

    Cleaned save/load posts, moved jwt to req headers

--- middleware/gamecontroller.js
+++ middleware/gamecontroller.js
@@ -1,29 +1,28 @@
-const jwt = require("jsonwebtoken");
 const GameState = require("../models/gamestate.js");
+const { verifyJwt } = require("./authcontroller.js");
 
 // Handle post requests to the save route
 async function savePost(req, res) {
-    const jwtToken = req.body.jwt;
-    if (jwtToken) {
-        jwt.verify(jwtToken, process.env.ACCESS_TOKEN_SECRET, async (error, decodedToken) => {
-            if (error) {
-                console.log(error.message)
-            } else {
-                gamestate = await GameState.findGame(decodedToken.id);
-                gamestate.currency = req.body.state.currency;
-                gamestate.clickUpgrades = req.body.state.clickUpgrades;
-                gamestate.idleUpgrades = req.body.state.idleUpgrades;
-                gamestate.clickMultiplier = req.body.state.clickMultiplier;
-                gamestate.idleMultiplier = req.body.state.idleMultiplier;
-                gamestate.save();
-                res.sendStatus(200);
-            }
-        });
+    try {
+        const jwtToken = verifyJwt(req.headers.authorization);
+        const game = await GameState.findByIdAndUpdate(jwtToken.id, { ...req.body.state });
+        if (game) { res.sendStatus(200); }
+        else { res.sendStatus(500); };
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(400);
     }
 }
 
 async function loadPost(req, res) {
-    console.log(req, res);
+    try {
+        const jwtToken = verifyJwt(req.headers.authorization);
+        const state = await GameState.getStateById(jwtToken.id);
+        res.status(201).json(state);
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(400);
+    }
 }
 
 module.exports = { savePost, loadPost }
