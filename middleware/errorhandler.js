a47f6361326bbc73d3249dfde2a4e3a4668ab757
Codrius <magefire342@gmail.com>
2023-08-25 02:26:00 -0400

    Adjusted error handler

--- middleware/errorhandler.js
+++ middleware/errorhandler.js
@@ -5,59 +5,49 @@ function user(error) {
     // If trying to log in to an email that doesn't exist, return error
     if (error.message.includes("Email does not exist")) {
         errors.email = "Email does not exist";
-        return errors;
     }
 
     // If log in password does not match email, return error
     if (error.message.includes("Email and password do not match")) {
         errors.password = "Email and password do not match";
-        return errors;
     }
 
     // If the specified email a user is trying to create already exists, return error
     if (error.code === 11000) {
         errors.email = "Email already exists";
-        return errors;
     }
 
     // If the user did not specify an email for registration, return error
     if (error.message.includes("Please enter an email")) {
         errors.email = "Please enter an email";
-        return errors;
     }
 
     // If the user did not specify an email for registration, return error
     if (error.message.includes("Please enter a username")) {
         errors.username = "Please enter a username";
-        return errors;
     }
 
     // If the user did not specify an email for registration, return error
     if (error.message.includes("Please enter a password")) {
         errors.password = "Please enter a password";
-        return errors;
     }
 
     // If the user entered an invalid email, return error
     if (error.message.includes("Please enter a valid email")) {
         errors.email = "Please enter a valid email";
-        return errors;
     }
 
     // If the username is > 12 characters, return error
     if (error.message.includes("Maximum username length is 12 characters")) {
         errors.username = "Maximum username length is 12 characters";
-        return errors;
     }
 
-    // If any other problem with user creation exists, return error
-    if (error.message.includes("user validation failed")) {
-        Object.values(error.errors).forEach(({ properties }) => {
-            errors[properties.path] = properties.message;
-        })
+    // If the username is > 12 characters, return error
+    if (error.message.includes("Minimum password length is 6 characters")) {
+        errors.password = "Minimum password length is 6 characters";
     }
 
-
+    return errors;
 }
 
 module.exports = { user };
